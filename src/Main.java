76f5bfb79ef15e2b4ccf76f7ab6f6eda3cfe268d
Hamza <hamza_razeq@hotmail.co.uk>
2022-09-14 17:23:22 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Customer customer1 = new Customer("Hamza", 0623746323, "Example Address", 1);
-        customer1.setAccountNumber(2653265);
+        Person customer1 = new Customer("Hamza", 0623746323, "Example Address", 1);
+        ((Customer)customer1).setAccountNumber(2653265);
 
         CurrentAccount currentAccount1 = new CurrentAccount("current", "Branch 1",
                 2222, 2653265, "Account 1",0,2, 200);
@@ -38,15 +38,12 @@ public class Main {
         sb.append("and the other");
         System.out.println(sb.toString());
 
+        String s = "Hello";
+        meth(s);
+        System.out.println(s);
+    }
 
-
-
-
-//        String days = "Friday";
-//        switch (days){
-//            case "Monday", "Tuesday" -> System.out.println("Today is mon or tue");
-//            case "Wednesday", "Thursday" -> System.out.println("Today is wed or thur");
-//            case "Friday" -> System.out.println("Today is Friday");
-//        }
+    public static void meth(String x){
+        x = x + " World";
     }
 }
